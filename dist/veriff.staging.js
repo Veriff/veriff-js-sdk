510f645ccf942cc69d73f6a8f4be96064654e985
Ilia Aphtsiauri <ilia.aptsiauri@veriff.me>
2018-02-08 12:32:35 +0400

    returns session id

--- dist/veriff.staging.js
+++ dist/veriff.staging.js
@@ -147,23 +147,28 @@ module.exports = bytesToUuid;
 "use strict";
 
 
-var _require = __webpack_require__(3),
+var uuid = __webpack_require__(3);
+
+var _require = __webpack_require__(6),
     createTemplate = _require.createTemplate;
 
-var createSession = __webpack_require__(5);
+var createSession = __webpack_require__(8);
 
 var Veriff = function Veriff(apiKey) {
-  var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-  var features = opts.features;
+  var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+      features = _ref.features,
+      _ref$id = _ref.id,
+      id = _ref$id === undefined ? uuid.v4() : _ref$id;
 
   return {
     apiKey: apiKey,
     person: {},
     features: features,
-    env: {"ENV":"staging","VERIFF_API_URL":"https://staging.veriff.me"}.ENV,
-    setOptions: function setOptions(_ref) {
-      var person = _ref.person,
-          features = _ref.features;
+    id: id,
+    env: {"ENV":"staging","VERIFF_API_URL":"https://staging.veriff.me/v1"}.ENV,
+    setOptions: function setOptions(_ref2) {
+      var person = _ref2.person,
+          features = _ref2.features;
 
       if (person) {
         this.person = Object.assign({}, this.person, person.givenName ? { givenName: person.givenName } : null, person.lastName ? { lastName: person.lastName } : null);
@@ -175,9 +180,9 @@ var Veriff = function Veriff(apiKey) {
     mount: function mount(parentId) {
       var _this = this;
 
-      var _ref2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-          label = _ref2.label,
-          submitValue = _ref2.submitValue;
+      var _ref3 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+          label = _ref3.label,
+          submitValue = _ref3.submitValue;
 
       var form = createTemplate(parentId, { person: this.person, label: label, submitValue: submitValue });
       form.onsubmit = function (e) {
@@ -189,14 +194,16 @@ var Veriff = function Veriff(apiKey) {
         var lastName = form.lastName ? form.lastName.value : null;
         _this.setOptions({ person: { givenName: givenName, lastName: lastName } });
         var data = {
+          id: _this.id,
           person: _this.person,
           features: _this.features
         };
-        createSession(_this.apiKey, data, function (err, data) {
+
+        createSession(_this.apiKey, data, function (err, response) {
           if (err) {
             throw new Error(err);
           }
-          window.location.href = data.verification.url;
+          window.location.href = response.verification.url;
         });
       };
     }
@@ -209,164 +216,8 @@ module.exports = Veriff;
 /* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
-
-
-var _require = __webpack_require__(4),
-    camelCaseToSlug = _require.camelCaseToSlug;
-
-var createInput = function createInput(opts) {
-  var type = opts.type,
-      value = opts.value,
-      name = opts.name;
-
-  var input = document.createElement('input');
-  input.setAttribute('type', type);
-  if (type === 'submit' && value) {
-    input.value = value;
-  }
-
-  input.setAttribute('class', 'veriff-' + type);
-  input.setAttribute('id', 'veriff-' + camelCaseToSlug(name));
-  input.setAttribute('name', name);
-  input.setAttribute('required', true);
-  return input;
-};
-var createLabel = function createLabel(value, labelFor) {
-  var label = document.createElement('label');
-  label.setAttribute('class', 'veriff-label');
-  label.setAttribute('id', 'veriff-label-' + camelCaseToSlug(labelFor));
-  label.setAttribute('htmlFor', labelFor);
-  label.innerHTML = value;
-  return label;
-};
-var createInputIfNeeded = function createInputIfNeeded(_ref) {
-  var container = _ref.container,
-      name = _ref.name,
-      label = _ref.label,
-      shouldRender = _ref.shouldRender;
-
-  if (!shouldRender) {
-    var inputLabel = createLabel(label, name);
-    var input = createInput({ type: 'text', name: name });
-    container.appendChild(inputLabel);
-    container.appendChild(input);
-  }
-};
-
-var createTemplate = function createTemplate(parentId) {
-  var _ref2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-      _ref2$label = _ref2.label,
-      label = _ref2$label === undefined ? {
-    givenName: 'Given name',
-    lastName: 'Last name'
-  } : _ref2$label,
-      _ref2$person = _ref2.person,
-      person = _ref2$person === undefined ? {
-    givenName: false,
-    lastName: false
-  } : _ref2$person,
-      _ref2$value = _ref2.value,
-      value = _ref2$value === undefined ? 'Start verification' : _ref2$value;
-
-  var parent = document.getElementById(parentId);
-  if (!parent) {
-    new Error('Element ' + parentId + ' does not exists');
-  }
-  var fragment = document.createDocumentFragment();
-  var container = document.createElement('form');
-
-  container.setAttribute('class', 'veriff-container');
-  container.setAttribute('name', 'veriff-form');
-
-  createInputIfNeeded({ container: container, name: 'givenName', label: label.givenName, shouldRender: person.givenName });
-  createInputIfNeeded({ container: container, name: 'lastName', label: label.lastName, shouldRender: person.lastName });
-
-  var submit = createInput({ type: 'submit', name: 'submitBtn', value: value });
-  container.appendChild(submit);
-
-  fragment.appendChild(container);
-  parent.appendChild(fragment);
-  return container;
-};
-
-module.exports = {
-  createTemplate: createTemplate
-};
-
-/***/ }),
-/* 4 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var util = exports;
-
-util.capitalize = function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-util.camelCaseToSlug = function camelCaseToSlug(str) {
-  return str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
-};
-
-util.camelCaseHuminize = function camelCaseHuminize(str) {
-  return util.capitalize(str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1 ').toLowerCase());
-};
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var uuid = __webpack_require__(6);
-
-var API_URL = {"ENV":"staging","VERIFF_API_URL":"https://staging.veriff.me"}.VERIFF_API_URL;
-
-var createSession = function createSession(apiKey, data, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", API_URL, true);
-  xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.setRequestHeader('x-auth-client', apiKey);
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status == '201') {
-        var resp = JSON.parse(xhr.responseText);
-        cb(null, resp);
-      } else {
-        cb(xhr.status, null);
-      }
-    }
-  };
-
-  var body = {
-    verification: {
-      id: uuid.v4(),
-      features: data.features,
-      person: {
-        firsName: data.person.givenName,
-        lastName: data.person.lastName
-      },
-      timestamp: new Date().toISOString()
-    }
-  };
-
-  var json = JSON.stringify(body);
-  xhr.send(json);
-};
-
-module.exports = createSession;
-
-/***/ }),
-/* 6 */
-/***/ (function(module, exports, __webpack_require__) {
-
-var v1 = __webpack_require__(7);
-var v4 = __webpack_require__(8);
+var v1 = __webpack_require__(4);
+var v4 = __webpack_require__(5);
 
 var uuid = v4;
 uuid.v1 = v1;
@@ -376,7 +227,7 @@ module.exports = uuid;
 
 
 /***/ }),
-/* 7 */
+/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var rng = __webpack_require__(0);
@@ -491,7 +342,7 @@ module.exports = v1;
 
 
 /***/ }),
-/* 8 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var rng = __webpack_require__(0);
@@ -524,6 +375,160 @@ function v4(options, buf, offset) {
 
 module.exports = v4;
 
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _require = __webpack_require__(7),
+    camelCaseToSlug = _require.camelCaseToSlug;
+
+var createInput = function createInput(opts) {
+  var type = opts.type,
+      value = opts.value,
+      name = opts.name;
+
+  var input = document.createElement('input');
+  input.setAttribute('type', type);
+  if (type === 'submit' && value) {
+    input.value = value;
+  }
+
+  input.setAttribute('class', 'veriff-' + type);
+  input.setAttribute('id', 'veriff-' + camelCaseToSlug(name));
+  input.setAttribute('name', name);
+  input.setAttribute('required', true);
+  return input;
+};
+var createLabel = function createLabel(value, labelFor) {
+  var label = document.createElement('label');
+  label.setAttribute('class', 'veriff-label');
+  label.setAttribute('id', 'veriff-label-' + camelCaseToSlug(labelFor));
+  label.setAttribute('htmlFor', labelFor);
+  label.innerHTML = value;
+  return label;
+};
+var createInputIfNeeded = function createInputIfNeeded(_ref) {
+  var container = _ref.container,
+      name = _ref.name,
+      label = _ref.label,
+      shouldRender = _ref.shouldRender;
+
+  if (!shouldRender) {
+    var inputLabel = createLabel(label, name);
+    var input = createInput({ type: 'text', name: name });
+    container.appendChild(inputLabel);
+    container.appendChild(input);
+  }
+};
+
+var createTemplate = function createTemplate(parentId) {
+  var _ref2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+      _ref2$label = _ref2.label,
+      label = _ref2$label === undefined ? {
+    givenName: 'Given name',
+    lastName: 'Last name'
+  } : _ref2$label,
+      _ref2$person = _ref2.person,
+      person = _ref2$person === undefined ? {
+    givenName: false,
+    lastName: false
+  } : _ref2$person,
+      _ref2$value = _ref2.value,
+      value = _ref2$value === undefined ? 'Start verification' : _ref2$value;
+
+  var parent = document.getElementById(parentId);
+  if (!parent) {
+    new Error('Element ' + parentId + ' does not exists');
+  }
+  var fragment = document.createDocumentFragment();
+  var container = document.createElement('form');
+
+  container.setAttribute('class', 'veriff-container');
+  container.setAttribute('name', 'veriff-form');
+
+  createInputIfNeeded({ container: container, name: 'givenName', label: label.givenName, shouldRender: person.givenName });
+  createInputIfNeeded({ container: container, name: 'lastName', label: label.lastName, shouldRender: person.lastName });
+
+  var submit = createInput({ type: 'submit', name: 'submitBtn', value: value });
+  container.appendChild(submit);
+
+  fragment.appendChild(container);
+  parent.appendChild(fragment);
+  return container;
+};
+
+module.exports = {
+  createTemplate: createTemplate
+};
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var util = exports;
+
+util.capitalize = function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+util.camelCaseToSlug = function camelCaseToSlug(str) {
+  return str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
+};
+
+util.camelCaseHuminize = function camelCaseHuminize(str) {
+  return util.capitalize(str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1 ').toLowerCase());
+};
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var API_URL = {"ENV":"staging","VERIFF_API_URL":"https://staging.veriff.me/v1"}.VERIFF_API_URL + '/sessions';
+
+var createSession = function createSession(apiKey, data, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", API_URL, true);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.setRequestHeader('x-auth-client', apiKey);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status == '201') {
+        var resp = JSON.parse(xhr.responseText);
+        cb(null, resp);
+      } else {
+        cb(xhr.status, null);
+      }
+    }
+  };
+
+  var body = {
+    verification: {
+      id: data.id,
+      features: data.features,
+      person: {
+        firsName: data.person.givenName,
+        lastName: data.person.lastName
+      },
+      timestamp: new Date().toISOString()
+    }
+  };
+
+  var json = JSON.stringify(body);
+  xhr.send(json);
+};
+
+module.exports = createSession;
 
 /***/ })
 /******/ ]);
