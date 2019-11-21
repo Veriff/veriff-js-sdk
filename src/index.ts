bdfafd6337552310de3d9ae2bca9a9b74b3b85f0
Maksim Tsvetkov <maxtsvetapple@gmail.com>
2019-11-21 17:49:08 +0200

    feat: set up css loader, turn css and js into one lib file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import './styles/style.css';
+import './styles/style.css';
 import { createTemplate } from './template';
 import { createSession } from './xhr';
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -18,7 +18,6 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                include: /node_modules/,
                 use: ['style-loader', 'css-loader'],
             },
         ],
